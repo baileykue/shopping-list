9383a4c1b26c014d541e4d2df97deecd5daeb2d9
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-14 18:04:50 -0800

    starting to set up auth and auth form

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
+import { signInUser, signUpUser } from '../../services/users';
+import AuthForm from '../components/AuthForm';
 
-export default function Auth() {
-  return <div>Auth</div>;
+export default function Auth({ type, setCurrentUser }) {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response =
+        type === 'signin'
+          ? await signInUser(email, password)
+          : await signUpUser(email, password);
+      setCurrentUser(response);
+    } catch {
+      setErrorMessage(
+        'Something went wrong. Please make sure all feilds are filled out correctly.'
+      );
+    }
+  };
+
+  return (
+    <>
+      <AuthForm
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+        errorMessage={errorMessage}
+      />
+    </>
+  );
 }
