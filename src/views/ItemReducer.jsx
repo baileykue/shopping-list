c89fa7c4ba567f245227c32dfd78d50aa20d1eec
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-14 17:52:44 -0800

    wrote reducer function, starting to set up components, decided to add supabase

--- src/views/ItemReducer.jsx
+++ src/views/ItemReducer.jsx
@@ -1,6 +1,14 @@
 const itemReducer = (state, action) => {
   switch (action.type) {
-    case '':
+    case 'add':
       return {};
+    case 'delete':
+      return {};
+    case 'edit':
+      return {};
+    default:
+      throw new Error(`this is not the ${action.type} i was looking for`);
   }
 };
+
+export { itemReducer };
--- src/views/Shopping.jsx
+++ src/views/Shopping.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import AddItem from '../components/AddItem';
+import ItemList from '../components/ItemList';
 
 export default function Shopping() {
-  return <div>Shopping</div>;
+  return (
+    <div>
+      <h1>shop til you drop</h1>
+      <AddItem />
+      <ItemList />
+    </div>
+  );
 }
