41b4485b326eca01ee4919824d64bd0de6f1b465
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-17 15:33:35 -0800

    removing a whole lot of fles to take it back to basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import Header from './components/Header';
-import { useUser } from './context/UserContext';
-import Auth from './views/Auth';
 import Grocery from './views/Grocery';
 
 export default function App() {
-  const { user } = useUser();
-
   return (
     <div>
-      <Header />
-      {user ? <Grocery /> : <Auth />}
+      <Grocery />
     </div>
   );
 }
