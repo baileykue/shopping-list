6bccf81f77605031601f4781370597b23350e1de
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-02-17 15:52:56 -0800

    moved reducer function back into the grocery view file

--- src/views/Grocery.jsx
+++ src/views/Grocery.jsx
@@ -1,44 +1,75 @@
 import { useReducer } from 'react';
-import { itemReducer } from '../components/ItemReducer';
 
 import AddItem from '../components/AddItem';
 import ItemList from '../components/ItemList';
-import { ListProvider } from '../context/ListContext';
+
+const initialItems = [
+  { id: 0, text: 'Cheese ', is_complete: false },
+  { id: 1, text: 'Wine', is_complete: false },
+  { id: 2, text: 'Walnuts', is_complete: false },
+];
 
 export default function Shopping() {
-  const initialState = [];
+  function itemReducer(items, action) {
+    switch (action.type) {
+      case 'add': {
+        return [
+          ...items,
+          {
+            id: action.id,
+            text: action.text,
+            is_complete: false,
+          },
+        ];
+      }
+      case 'edit': {
+        return items.map((item) => {
+          if (item.id === action.task.id) {
+            return action.task;
+          }
+          return item;
+        });
+      }
+      case 'delete': {
+        return items.filter((item) => item.id !== action.id);
+      }
+      default: {
+        throw Error(
+          `this is not the action i was looking for: ${action.type} not recognized`
+        );
+      }
+    }
+  }
 
-  const [items, dispatch] = useReducer(itemReducer, initialState);
+  const [items, dispatch] = useReducer(itemReducer, initialItems);
 
   const handleAdd = (text) => {
     dispatch({
-      type: 'add',
+      type: 'added',
       id: items.length + 1,
-      item: text,
-    });
-  };
-
-  const handleDelete = (itemId) => {
-    dispatch({
-      type: 'delete',
-      id: itemId,
+      text,
     });
   };
 
   const handleEdit = (task) => {
     dispatch({
-      type: 'edit',
+      type: 'changed',
       task,
+    });
+  };
+
+  const handleDelete = (taskId) => {
+    dispatch({
+      type: 'deleted',
+      id: taskId,
     });
   };
 
   return (
     <div>
-      <h1>shop til you drop</h1>
-      <ListProvider>
-        <AddItem handleAdd={handleAdd} />
-        <ItemList handleEdit={handleEdit} handleDelete={handleDelete} />
-      </ListProvider>
+      <h1>grocery shop til you drop</h1>
+      <AddItem handleAdd={handleAdd} />
+      <ItemList handleEdit={handleEdit} handleDelete={handleDelete} />
     </div>
   );
 }
